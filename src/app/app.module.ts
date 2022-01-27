9d0096e6f3966f2053e77d9376289e65eece5433
Nimatullah Razmjo <nimat.razmjo@gmail.com>
2022-01-27 10:00:45 +0430

    Add http interceptor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { Router } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CredentialsInterceptor } from './interceptors/credentials.interceptor';
 import { PublicModule } from './public/public.module';
 import { SecureModule } from './secure/secure.module';
 
@@ -17,7 +19,13 @@ import { SecureModule } from './secure/secure.module';
     PublicModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: CredentialsInterceptor,
+      multi: true
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
